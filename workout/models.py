bf4f0d5e71e3fa93c7e8170e207693ac74825541
Daniel R <daniel.rincon-m@mail.escuelaing.edu.co>
2020-11-25 13:31:40 -0500

    Modelos creados y subidos a la bd

--- workout/models.py
+++ workout/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField(max_length=500, blank=True)
+
+
+class Ejercicio(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField(max_length=500, blank=True)
+    prioridad = models.IntegerField(default=0)
+    imagen = models.ImageField(upload_to="media/", null=True)
+    peso = models.FloatField(default=0.0)
+    se_mide_en_reps = models.BooleanField(default=True)
+    intensidad_normal = models.IntegerField(default=0)
+    intensidad_falla = models.IntegerField(default=0)
+    categorias = models.ManyToManyField(Categoria)
+
+
+class Rutina(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField(max_length=500, blank=True)
+    fecha_de_creacion = models.DateTimeField(auto_now_add=True)
+    favorito = models.BooleanField(default=False)
+    ejercicios = models.ManyToManyField(Ejercicio)
