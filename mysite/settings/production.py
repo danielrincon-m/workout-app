64d8ee4f7106378a2f1143f6090891f107dc8d71
Daniel R <daniel.rincon-m@mail.escuelaing.edu.co>
2020-11-26 10:49:23 -0500

    dworkout domain

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -6,4 +6,4 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".herokuapp.com"]
+ALLOWED_HOSTS = [".herokuapp.com", ".dworkout.tk"]
